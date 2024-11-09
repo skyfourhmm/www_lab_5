6767229cb5768727afbe34e2f341095bfd449d45
skyfourhmmm <nguyenthientu413.com>
2024-11-09 15:56:47 +0700

    fixing

---
+++ src/main/java/vn/edu/iuh/fit/NguyenThienTu21111951Lab5Application.java
@@ -0,0 +1,12 @@
+package vn.edu.iuh.fit;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenThienTu21111951Lab5Application {
+    public static void main(String[] args) {
+        SpringApplication.run(NguyenThienTu21111951Lab5Application.class, args);
+    }
+}
